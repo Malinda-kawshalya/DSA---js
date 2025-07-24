4df94023658b79295b8f5f1ab3737f7e03680ae8
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-07-24 12:40:34 +0530

    selection sort

---
+++ selectionSort.js
@@ -0,0 +1,22 @@
+let selectionSort = (arr) =>
+{
+    
+    for(let i=0; i<arr.length-1; i++)
+    {
+        let minIndex=i;
+
+        for(let j=i+1 ; j<arr.length ; j++)
+        {
+          if (arr[j]<arr[minIndex])
+          {
+            minIndex=j;
+          }
+        }
+        if (minIndex !==i)
+        {
+            [arr[i],arr[minIndex]]= [arr[minIndex], arr[i]]
+        }
+    }
+    return arr;
+}
+console.log(selectionSort([10,200,350,40,50,60],50))
