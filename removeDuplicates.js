75a6c531fe7c6ae1b39a59a80749c9583a126f66
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-08-04 19:40:23 +0530

    duplicates remove

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,6 +1,7 @@
 let arr=[1,2,2,5,6,6,7]
 
 
+
 function removeDuplicates(array)
 { 
     return array.filter  //This goes through each item in the array and decides whether to keep it or not.
@@ -12,12 +13,29 @@ function removeDuplicates(array)
 
 console.log("filter method " +removeDuplicates(arr))
 
-//e1: the current element in the array. 
+// e1: the current element in the array. 
 // index: the position of e1 in the array.
-//array.indexOf(e1): finds the first place this value appears in the array
-
+// array.indexOf(e1): finds the first place this value appears in the array
 // for the first 2 (at index 1): array.indexOf(2) is 1 → keep it ✅
-
 // For the second 2 (at index 2): array.indexOf(2) is still 1 → skip it ❌
 
+//--------------------------------------------------------------------------------------------------------------
 
+function removeDuplicates2(array) {
+  const unique = [];
+
+  array.forEach((element) => {
+    if (!unique.includes(element)) {
+      unique.push(element);
+    }
+  });
+
+  return unique;
+}
+console.log("foreach method " +removeDuplicates2(arr))
+
+//go through the each element and checks this value already include in unique array and if not add to it
+//--------------------------------------------------------------------------------------------------------------
+
+
+function 
